d7785e3b127da64b227071cb27a4f8298194ff17
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2024-08-02 14:23:07 +0530

    Add files via upload

---
+++ getattribute/app.js
@@ -0,0 +1,39 @@
+//            get attribute
+// let sam=document.querySelector('a');
+
+// console.log(sam.getAttribute('href'));
+
+// console.log(sam.getAttribute('class'));
+
+
+//            set attribute
+
+
+
+// let sam=document.querySelector('a')
+
+// sam.setAttribute('href','http://www.facebook.com');
+// console.log(sam.getAttribute('href'));
+
+
+
+// let sam=document.querySelector('h1')
+
+// sam.setAttribute('class','rishabh')
+
+// let sam=document.querySelector('input')
+
+// // sam.setAttribute('type','checkbox');
+
+// sam.setAttribute('type','email');
+
+
+
+
+
+
+
+
+
+
+
