b16c60b61ba8cdcec642244143fbe9ef992adcf8
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2024-08-02 11:58:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,89 @@
+
+// console.log(document.getElementsByTagName('h1'));
+
+// let h1arr=document.getElementsByTagName('h1');
+
+// console.log(h1arr[0]);
+// console.log(h1arr[1]);
+
+// for(let item of h1arr)
+// {
+//     console.log(item);
+// }
+
+
+// let parr=document.getElementsByTagName('p');
+
+// for(let item of parr)
+// {
+//     console.log(item);
+// }
+
+
+
+// let parr=document.getElementsByTagName('p');
+
+// for(let item of parr)
+// {
+//     item.style.color='red';
+//     item.style.border='4px solid  blue';
+// }
+
+
+//                                                  getElementByid
+
+
+// let a=document.getElementById('sam');
+
+// a.style.fontSize='70px'
+
+
+
+//                                                  getElemetsByClassName
+
+
+// let carr=document.getElementsByClassName('rish');
+
+// for(let item of carr)
+// {
+//     item.style.color='orange';
+// }
+
+
+
+
+//                                                    querySelector
+
+
+// let sam=document.querySelector('#sam');
+// console.log(sam);
+
+
+// let sam=document.querySelector('.rish');
+// console.log(sam);
+
+
+
+//                                                  querySelectorAll
+
+
+
+
+// let sam=document.querySelectorAll('h1');
+
+// console.log(sam);
+
+
+// let sam=document.querySelectorAll('.rish');
+
+// console.log(sam);
+
+
+
+
+
+
+
+
+
+
