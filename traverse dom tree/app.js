aa8c645e7c57441f90b88591047a9f7ad0396c26
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2024-08-03 11:57:12 +0530

    Add files via upload

---
+++ traverse dom tree/app.js
@@ -0,0 +1,42 @@
+
+
+
+// let para1=document.querySelector('#para1')
+
+// let arti=document.querySelector('#arti')
+
+// //    parentElement
+
+// // console.log(para1.parentElement.parentElement.parentElement.parentElement.parentElement)// html ka parent null hota h 
+// console.log(arti.children)
+// // children
+
+// let coll=arti.children
+
+// for(let item of coll)
+// {
+
+//     item.style.fontSize='24px'
+
+// }
+
+
+//nextElementsibling
+
+let sam=document.querySelector('#div3')
+
+// console.log(sam.nextElementSibling)
+
+//previouselementsibling
+
+// console.log(sam.previousElementSibling)
+
+
+
+
+
+
+
+
+
+
