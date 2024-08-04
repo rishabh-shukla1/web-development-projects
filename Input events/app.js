068ad7e4da583b70469cfcd082fc3108c22c537a
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2024-08-04 13:25:20 +0530

    Add files via upload

---
+++ Input events/app.js
@@ -0,0 +1,24 @@
+
+
+// let btn=document.querySelector('input')
+
+
+// function sam(event)
+// {
+//     console.log(event.target.value)
+//     // console.log("enter ho gaya");
+// }
+// btn.addEventListener('input',sam)
+
+
+
+// adding the content to h2 tag to the input
+
+// let btn=document.querySelector('input')
+
+// let h2tag=document.querySelector('h2')
+
+// btn.addEventListener('input',function(e)
+// {
+//     h2tag.innerText=e.target.value;
+// })
