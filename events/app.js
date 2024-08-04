f18b8f67349bdc666de04864dc98282433a0df0e
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2024-08-04 11:24:47 +0530

    Add files via upload

--- events/app.js
+++ events/app.js
@@ -1,11 +1,137 @@
+// inline way
+
+// function dosomething(){
+//     console.log('maine buttom daba dia jo karna h kar lo ');
+// }
 
 
-function dosomething(){
-    console.log('maine buttom daba dia jo karna h kar lo ');
-}
+// let btn=document.querySelector('.btn')
+
+// console.log(btn)
+// console.dir(btn);
 
 
-let btn=document.querySelector('.btn')
+// 2nd way
 
-console.log(btn)
-console.dir(btn);
+
+// let btn=document.querySelector('button')
+// btn.onclick=function(){
+//     console.log('daba dia')
+// }
+
+
+//onmouseenter
+
+// btn.onmouseenter=function()
+// {
+//     btn.style.color='red';
+// }
+
+// // onmouseleave
+
+// btn.onmouseleave=function()
+// {
+//     btn.style.color='blue';
+// }
+
+
+
+
+// let btn=document.querySelector('button')
+
+
+
+// function one()
+// {
+//     console.log('one');
+// }
+
+// function two()
+// {
+//     console.log('two');
+// }
+
+
+// btn.onclick=one // this will not work
+// btn.onclick=two;// this will work 
+
+
+// onclick we can only work with one funtion 
+
+
+// solution by using eventListner
+
+
+
+// let btn=document.querySelector('button')
+
+
+
+// function one()
+// {
+//     console.log('one');
+// }
+
+// function two()
+// {
+//     console.log('two');
+// }
+
+// btn.addEventListener('click',one)
+// btn.addEventListener('click',two)
+
+
+
+// question -:body ka background color  change ho jae on click 
+
+
+
+// let btn=document.querySelector('button')
+
+// btn.style.backgroundColor='yellow';
+
+
+// function change()
+// {
+//     btn.style.backgroundColor='pink'
+// }
+
+
+// btn.onclick=change;
+
+
+
+// body ka color change karana h click karne par
+
+
+
+// let btn=document.querySelector('button')
+
+// let onlybody=document.querySelector('body')
+
+// btn.addEventListener('click',function()
+// {
+//    onlybody.style.backgroundColor='blue'
+// })
+
+
+
+
+
+//onclick-> ek bar dabane se kam karta h
+
+
+// ondblclick-> 2 bar dabane se kam karta h
+
+
+// let btn=document.querySelector('button')
+// btn.addEventListener('dblclick',function(){
+//     console.log('2 bar dabaya h ')
+// })
+
+
+
+
+
+
+
