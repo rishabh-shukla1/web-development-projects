84046b8cee5e209dc844a9afa1f6a66bf81ede99
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2024-08-08 23:18:06 +0530

    Add files via upload

---
+++ canavas/app.js
@@ -0,0 +1,50 @@
+
+
+let canvas=document.querySelector('canvas')
+
+let brush=canvas.getContext('2d')
+
+// brush.fillRect(100,50,100,50)
+
+// brush.fillStyle='red'; //pehle paint me duba deta hu uske bad print karo fillrect
+
+// brush.fillRect(100,50,100,50)
+
+// brush.strokeRect(100 ,50 ,100 ,50)
+
+// brush.beginPath();
+// brush.moveTo(40,40);
+// brush.lineTo(100,100);
+// brush.lineTo(30,200);
+// brush.lineTo(40,40);
+
+// brush.stroke();
+
+
+
+// draw triange
+
+// brush.beginPath();
+
+// brush.moveTo(100,100);
+// brush.lineTo(150,150);
+// brush.lineTo(50,150);
+// brush.lineTo(100,100);
+// // brush.stroke();
+
+// brush.fill();
+// brush.closePath();
+
+
+
+
+// drawing text
+brush.font='20px monospace'
+
+brush.fillStyle='red'
+
+brush.fillText('Rishabh Shukla',50,50);
+
+
+
+
